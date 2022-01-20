b5b80983cdd757daae7a571e4a8beafde1035ae5
Josh Dun <68704602+joshdun31@users.noreply.github.com>
2022-01-20 21:09:08 +0530

    Update users.js

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,5 +1,5 @@
 import dbConnect from '../../lib/dbConnect'
-import User from '../../models/User'
+import User from '../../models/user'
 
 export default async function handler (req, res) {
   const { method } = req
