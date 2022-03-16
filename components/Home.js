2a65aad49cb45d45e7049b2c886a8825ed3c4e8c
Naresh E <nareshe64@gmail.com>
2022-03-16 14:20:04 +0530

    ga removed

--- components/Home.js
+++ components/Home.js
@@ -3,7 +3,6 @@ import styles from "../scss/components/home.module.scss";
 import Head from "next/head";
 import Poster from "./atoms/Poster";
 import ScrollContainer from "react-indiana-drag-scroll";
-import * as ga from '../lib/ga'
 
 function Home({ movieData, tvData, base_url }) {
     const router = useRouter();
@@ -29,21 +28,6 @@ function Home({ movieData, tvData, base_url }) {
             );
         }
         };
-
-        useEffect(() => {
-            const handleRouteChange = (url) => {
-              ga.pageview(url)
-            }
-            //When the component is mounted, subscribe to router changes
-            //and log those page views
-            router.events.on('routeChangeComplete', handleRouteChange)
-        
-            // If the component is unmounted, unsubscribe
-            // from the event with the `off` method
-            return () => {
-              router.events.off('routeChangeComplete', handleRouteChange)
-            }
-          }, [router.events])
 
     return (
         <>
